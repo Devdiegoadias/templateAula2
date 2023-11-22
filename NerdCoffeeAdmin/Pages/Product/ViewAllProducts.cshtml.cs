fe9a32ef442bb544a905e6a8af39f67bb88f8406
Diego Tadeu Martins Acioly Ribeiro Dias <diego.dias@prof.infnet.edu.br>
2023-11-22 07:44:15 -0300

    uploading

---
+++ NerdCoffeeAdmin/Pages/Product/ViewAllProducts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NerdCoffeeAdmin.Pages.Product
+{
+    public class ViewAllProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
